e08b83a2256da0861efac679b4bf134602ad3546
Gastón Gutierrez <gutigas96@gmail.com>
2025-11-25 13:16:40 -0300

    a lot of stuff

---
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -0,0 +1,28 @@
+import { Catch, ArgumentsHost, UnauthorizedException, ExceptionFilter } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { RpcException } from '@nestjs/microservices';
+
+@Catch(RpcException)
+export class RpcCustomExceptionFilter implements ExceptionFilter {
+
+    catch(exception: RpcException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse();
+        const rpcError = exception.getError();
+
+        if(
+            typeof rpcError === 'object' &&
+            'status' in rpcError &&
+            'message' in rpcError
+        ) {
+            const status = rpcError.status && !isNaN(+rpcError.status) ? +rpcError.status : 400
+            response.status(status).json(rpcError)
+        }
+
+        response.status(400).json({
+            status: 400,
+            message: rpcError
+        })
+    }
+
+}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,26 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
+import { ChangeOrderStatusDto } from './dto';
 
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('OrdersService');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected')
+  }
+
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    return this.order.create({
+      data: createOrderDto
+    })
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+    const { page, limit, status } = orderPaginationDto
+
+    const totalOrders = await this.order.count({
+      where: {
+        status
+      }
+    })
+    const totalPages = Math.ceil(totalOrders / limit!)
+
+    return {
+      data: await this.order.findMany({
+        where: { status },
+        skip: (page! - 1) * limit!,
+        take: limit
+      }),
+      meta: {
+        totalPages: totalPages,
+        page: page,
+        totalOrders: totalOrders
+      }
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string) {
+    const order = await this.order.findUnique({ where: { id } })
+
+    if (!order) {
+      throw new RpcException({
+        status: HttpStatus.NOT_FOUND,
+        message: `Order with id ${id} not found`
+      })
+    }
+
+    return order
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
+  async changeStatus(changeOrderStatusDto: ChangeOrderStatusDto) {
+    const { id, status } = changeOrderStatusDto;
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+    const order = await this.findOne(id)
+
+    if (!order && order) {
+      throw new RpcException({
+        status: HttpStatus.NOT_FOUND,
+        message: `Order with id ${id} not found`
+      })
+    }
+
+    return this.order.update({
+      where: { id },
+      data: { status: status }
+    })
   }
 }
